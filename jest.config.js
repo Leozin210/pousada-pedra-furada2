5933d3dcaa89611a059e669094ea90da376c14fb
Leozin210 <151797402+Leozin210@users.noreply.github.com>
2025-10-17 09:03:17 -0300

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+ testEnvironment: "jsdom",
+ setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+ moduleNameMapper: {
+ "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+ },
+ transform: {
+ "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+ },
+ testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"]
+};
+module.exports = config; 
